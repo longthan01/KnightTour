89705ac42fc545c1ff418c5816b4ba8a82def858
SM\SM <temp.smalldragon@gmail.com>
2017-04-21 09:01:50 +0700

    Initial

---
+++ DuongDiConNgua/AppCodes/ChessSquare.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DuongDiConNgua.AppCodes
+{
+    public class ChessSquare : PictureBox
+    {
+        public int SquareSize { get; set; }
+        public Image Img { get; set; }
+        public ChessSquare(int size, Image img)
+        {
+            this.SquareSize = size;
+            this.Img = img;
+            this.Width = size;
+            this.Height = size;
+            this.Image = img;
+            this.Margin = new Padding(1,1,1,1);
+            this.MouseEnter += ChessSquare_MouseEnter;
+            this.MouseLeave += ChessSquare_MouseLeave;
+        }
+
+        private void ChessSquare_MouseLeave(object sender, EventArgs e)
+        {
+            this.Image = Img;
+        }
+
+        private void ChessSquare_MouseEnter(object sender, EventArgs e)
+        {
+            this.Image = Properties.Resources.RedHorse;
+        }
+
+        public void SetImage(Image img)
+        {
+            this.Image = img;
+        }
+    }
+}
---
+++ DuongDiConNgua/Form1.cs
@@ -0,0 +1,58 @@
+﻿using DuongDiConNgua.AppCodes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DuongDiConNgua
+{
+    public partial class Form1 : Form
+    {
+        private int ChessBoardSize { get; set; }
+        public Form1()
+        {
+            InitializeComponent();
+            this.WindowState = FormWindowState.Maximized;
+            this.ChessBoardSize = 8;
+        }
+        private void CreateLayout()
+        {
+            chessBoardPanel = new FlowLayoutPanel();
+            chessBoardPanel.Size = new Size(this.Size.Height - 50, this.Size.Height - 50);
+            chessBoardPanel.AutoSize = false;
+            chessBoardPanel.Location = new Point(this.Width - chessBoardPanel.Width - 50, 0);
+            this.Controls.Add(chessBoardPanel);
+        }
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            CreateLayout();
+            Image first = Properties.Resources.Red;
+            Image second = Properties.Resources.White;
+            int s= this.chessBoardPanel.Width;
+            int size = (s - 50) / ChessBoardSize;
+            for (int i = 0; i < ChessBoardSize; i++)
+            {
+                for (int j = 0; j < ChessBoardSize; j++)
+                {
+                    this.chessBoardPanel.Controls.Add(new ChessSquare(size, first));
+                    SwapImg(ref first, ref second);
+                }
+                SwapImg(ref first, ref second);
+                Application.DoEvents();
+            }
+        }
+        void SwapImg(ref Image a, ref Image b)
+        {
+            Image t = a;
+            a = b;
+            b = t;
+        }
+        private FlowLayoutPanel chessBoardPanel;
+    }
+}
